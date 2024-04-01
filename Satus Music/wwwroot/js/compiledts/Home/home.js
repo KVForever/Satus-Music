3a33bca5fae45fe734b3b4fbc7024641426180c5
Kal Vassar <kvassar@hbs.net>
2024-04-01 10:25:44 -0500

    Added top song animation for scrolling.

--- Satus Music/wwwroot/js/compiledts/Home/home.js
+++ Satus Music/wwwroot/js/compiledts/Home/home.js
@@ -8,21 +8,24 @@ async function home() {
     const track = new Track(Auth.token);
     const profile = await user.currentProfile();
     document.getElementById("username").innerText = profile.display_name;
-    const songs = await user.usersTopItems("tracks", "medium_term");
-    const artists = await user.usersTopItems("artists", "medium_term");
+    const songs = await user.usersTopItems("tracks", "short_term");
+    const artists = await user.usersTopItems("artists", "short_term");
     /* let imageStart = Math.floor(Math.random() * 19);*/
     let imageStart = 0;
+    /*get initial image information.*/
     let displayImage = songs.items[imageStart].album.images[0];
     let colorImage = songs.items[imageStart].album.images[2];
     let song = songs.items[imageStart].artists[0].name;
     let artist = songs.items[imageStart].name;
+    /*set the image and desription.*/
     Images.setImageWithDescription("home-canvas", displayImage, "top-track-name", song, "top-track-artist-name", artist);
+    /*grab initial image colors.*/
     let topColors = await Images.grabImageColors("home-color-canvas", colorImage);
-    /* let test = await Images.grabImageColorsTwo("home-color-canvas", colorImage);*/
+    /*set the text to black or white*/
     var textColor = blackOrWhite(topColors);
     document.documentElement.style.setProperty("--site-text-color", textColor);
     document.documentElement.style.setProperty("--home-canvas-border-color", textColor);
-    /*let siteBackgroundColor = `rgb(${topColors[0]})`;*/
+    /*set initial background colors*/
     let siteBackgroundColor = `radial-gradient(18% 28% at 24% 50%, rgba(${topColors[12]}, 1) 7%, #073AFF00 100%),radial-gradient(18% 28% at 18% 71%, rgba(${topColors[10]}, 1) 6%, #073AFF00 100%),
     radial-gradient(70% 53% at 36% 76%, rgba(${topColors[9]}, 1) 0%, #073AFF00 100%),radial-gradient(42% 53% at 15% 94%, rgba(${topColors[8]}, 1) 7%, #073AFF00 100%),
     radial-gradient(42% 53% at 34% 72%, rgba(${topColors[7]}, 1) 7%, #073AFF00 100%),radial-gradient(18% 28% at 35% 87%, rgba(${topColors[6]}, 1) 7%, #073AFF00 100%),
@@ -31,56 +34,9 @@ async function home() {
     linear-gradient(125deg,  rgba(${topColors[0]}, 1) 1%,  rgba(${topColors[1]}, 1) 100%)`;
     document.documentElement.style.setProperty("--home-background-color", siteBackgroundColor);
     document.getElementById("greeting-background").style.background = `linear-gradient(180deg, rgba(${topColors[0]}, 1) 14%, rgba(${topColors[0]}, 1) 33%, rgba(${topColors[0]}, 0.9) 50%, rgba(${topColors[0]}, ${topColors[0]}, 0.00001) 85%)`;
-    let artistOneImage = artists.items[0].images[0];
-    let artistOneColorImage = artists.items[0].images[2];
-    let artistOneName = artists.items[0].name;
-    let artistTwoImage = artists.items[1].images[0];
-    let artistTwoColorImage = artists.items[1].images[2];
-    let artistTwoName = artists.items[1].name;
-    let artistThreeImage = artists.items[2].images[0];
-    let artistThreeColorImage = artists.items[2].images[2];
-    let artistThreeName = artists.items[2].name;
-    Images.setArtistImage("top-artist-one", artistOneImage, "top-artist-one-name", artistOneName);
-    let artistOneColors = await Images.grabImageColors("top-artist-one-color-canvas", artistOneColorImage);
-    document.getElementById("top-artist-one").style.boxShadow = `2px 1px 12px 15px rgba(${artistOneColors[0]}, 1)`;
-    Images.setArtistImage("top-artist-two", artistTwoImage, "top-artist-two-name", artistTwoName);
-    let artistTwoColors = await Images.grabImageColors("top-artist-two-color-canvas", artistTwoColorImage);
-    document.getElementById("top-artist-two").style.boxShadow = `2px 1px 12px 15px rgba(${artistTwoColors[0]}, 1)`;
-    Images.setArtistImage("top-artist-three", artistThreeImage, "top-artist-three-name", artistThreeName);
-    let artistThreeColors = await Images.grabImageColors("top-artist-three-color-canvas", artistThreeColorImage);
-    document.getElementById("top-artist-three").style.boxShadow = `2px 1px 12px 15px rgba(${artistThreeColors[0]}, 1)`;
+    /*repeate getting image, setting image and information, getting image colors, and setting background colors.*/
     repeat();
-    //track
-    //var tableBackgroundColors: string[] = [];
-    //for (let i = 0; i < 10; i++){
-    //    document.getElementById(`artist${i + 1}`).innerHTML = `${songs.items[i].album.artists[0].name}`;
-    //    document.getElementById(`song${i + 1}`).innerHTML = `${songs.items[i].name}`;
-    //    document.getElementById(`popularity${i + 1}`).innerHTML = `${songs.items[i].popularity}`;
-    //    let trackImage = songs.items[i].album.images[2];
-    //    Images.setImage(`cover${i + 1}`, trackImage);
-    //    let topColors = await Images.grabImageColors(`cover${i + 1}`, trackImage);
-    //    document.getElementById(`background${i + 1}`).style.color = "black";
-    //    let gradiant = `linear-gradient(-90deg, rgba(${topColors[0]}, .8) 0%, rgba(${topColors[2]}, .8) 100%)`;
-    //    tableBackgroundColors.push(gradiant);
-    //};
-    //var tableRows = document.querySelectorAll('#song-table tr');
-    //var i = 1
-    //tableRows.forEach(function (row) {
-    //    tableRows[i].addEventListener('mouseover', function () {
-    //        var id = row.id
-    //        let whichOne = Number(id.slice(10))
-    //        var color = tableBackgroundColors[whichOne];
-    //        this.style.backgroundImage = color;
-    //        let textColor = blackOrWhite(color);
-    //        this.style.color = textColor;
-    //    });
-    //    tableRows[i].addEventListener('mouseout', function () {
-    //        this.style.backgroundImage = 'none'; // Reset background color on mouseout
-    //        this.style.backgroundColor = "white";
-    //        this.style.color = "black";
-    //    });
-    //    i++
-    //});
+    //use users top songs that I got at the start and set a few canvases with the song image along with getting the descripiton and some other information about the song.
     for (let i = 0; i < songs.items.length / 2; i++) {
         let trackImage = songs.items[i].album.images[0];
         let trackColorImage = songs.items[i].album.images[2];
@@ -91,28 +47,74 @@ async function home() {
         Images.setImageWithDescription(`top-song-${i + 1}`, trackImage, `top-song-${i + 1}-name`, trackName, `top-song-${i + 1}-artist-name`, trackArtist);
         let songColors = await Images.grabImageColors(`top-song-${i + 1}-color-canvas`, trackColorImage);
         document.getElementById(`top-song-${i + 1}`).style.boxShadow = `2px 1px 12px 15px rgba(${songColors[0]}, 1)`;
-        document.getElementById(`spot-${i + 1}-stats`).innerHTML = `Danceability: ${features.danceability} <br> Energy: ${features.energy} <br> Instrumentalness: ${features.instrumentalness}`;
+        var danceability = document.getElementById(`spot-${i + 1}-stats`).getElementsByClassName("progress-bar")[0];
+        danceability.style.width = `${(features.danceability * 100)}%`;
+        var Energy = document.getElementById(`spot-${i + 1}-stats`).getElementsByClassName("progress-bar")[1];
+        Energy.style.width = `${(features.energy * 100)}%`;
+        var Instrumentalness = document.getElementById(`spot-${i + 1}-stats`).getElementsByClassName("progress-bar")[2];
+        Instrumentalness.style.width = `${(features.instrumentalness * 100)}%`;
     }
+    //get all of the song canvases
     const observedElements = document.querySelectorAll('.song-image');
+    //this is target for the intersection observer.
     const target = document.getElementById("card-container");
-    const observer = new IntersectionObserver((entries) => {
+    //what I will set to get the images to get larger and smaller when scrolling left and right.
+    let prevRatio = 0.0;
+    let increasingWidth = "calc(65% * ratio)";
+    /*    let decreasingWidth = "calc(65% * ratio)";*/
+    //this makes an array of numbers that will trigger the intersection observer into performing the calculations to change the width.
+    function buildThresholdList() {
+        let thresholds = [];
+        let numSteps = 20;
+        for (let i = 1.0; i <= numSteps; i++) {
+            let ratio = i / numSteps;
+            thresholds.push(ratio);
+        }
+        thresholds.push(0);
+        return thresholds;
+    }
+    //creates the intersection oberver with the options that are spcified.
+    function createObserver() {
+        let observer;
+        let options = {
+            root: target,
+            rootMargin: "0% -33.3333333% 0% -33.33333333%",
+            threshold: buildThresholdList()
+        };
+        observer = new IntersectionObserver(handleIntersect, options);
+        observedElements.forEach(element => {
+            observer.observe(element);
+        });
+    }
+    //this will check if the amount that the image is intersecting with the target is getting larger and smaller and will make the width of the image smaller or larger deppending on that. 
+    // it willl also make the image flippable if the image is intersecting with the target.
+    function handleIntersect(entries, observer) {
         entries.forEach((entry) => {
             const element = entry.target;
-            if (entry.isIntersecting) {
-                element.style.width = "100%";
+            if (entry.intersectionRatio > .4) {
             }
-            else {
-                element.style.width = "65%";
+            if (entry.intersectionRatio > prevRatio) {
+                element.classList.remove("reduce-card");
+                let increase = (entry.intersectionRatio / 2 + 1).toString();
+                let width = increasingWidth.replace("ratio", increase);
+                element.style.width = width;
             }
+            prevRatio = entry.intersectionRatio;
+            if (entry.isIntersecting < .5) {
+                element.classList.add("reduce-card");
+            }
+            console.log(prevRatio);
         });
-    }, {
-        root: target,
-        rootMargin: "0% -33.33% 0% -33.33%",
-        threshold: .50
+    }
+    createObserver();
+    var elements = document.querySelectorAll(".flip-card-btn");
+    elements.forEach((entry) => {
+        entry.addEventListener("click", () => {
+            entry.parentElement.parentElement.parentElement.parentElement.classList.add("flippable");
+        });
     });
-    observedElements.forEach(element => {
-        observer.observe(element);
-    });
+    //this code will allow the user to drag left and right in the card container.
+    //---------------------------------------------------------------------------------------------------------------
     const cards = document.getElementById("card-container");
     let isDown = false;
     let startX;
@@ -136,30 +138,25 @@ async function home() {
         const walk = (x - startX); //scroll-fast       
         cards.scrollLeft = scrollLeft - walk;
     });
-    //let trackOne = songs.items[0].album.images[0];
-    //let trackOneColorImage = songs.items[0].album.images[2];
-    //let trackOneName = songs.items[0].artists[0].name;
-    //let trackOneArtist = songs.items[0].name;
-    //let trackId = songs.items[0].id;
-    //let features = await track.tracksFeatures(trackId);
-    //let trackTwo = songs.items[1].album.images[0];
-    //let trackTwoColorImage = songs.items[1].album.images[2];
-    //let trackTwoName = songs.items[1].artists[0].name;
-    //let trackTwoArtist = songs.items[1].name;
-    //let trackThree = songs.items[2].album.images[0];
-    //let trackThreeColorImage = songs.items[2].album.images[2];
-    //let trackThreeName = songs.items[2].artists[0].name;
-    //let trackThreeArtist = songs.items[2].name;
-    //Images.setImageWithDescription("top-song-one", trackOne, "top-song-one-name", trackOneName, "top-song-one-artist-name", trackOneArtist);
-    //let songOneColors = await Images.grabImageColors("top-song-one-color-canvas", trackOneColorImage);
-    //document.getElementById("top-song-one").style.boxShadow = `2px 1px 12px 15px rgba(${songOneColors[0]}, 1)`;
-    //Images.setImageWithDescription("top-song-two", trackTwo, "top-song-two-name", trackTwoName, "top-song-two-artist-name", trackTwoArtist);
-    //let songTwoColors = await Images.grabImageColors("top-song-two-color-canvas", trackTwoColorImage);
-    //document.getElementById("top-song-two").style.boxShadow = `2px 1px 12px 15px rgba(${songTwoColors[0]}, 1)`;
-    //document.getElementById("spot-1-stats").innerHTML = `Danceability: ${features.danceability} <br> Energy: ${features.energy} <br> Instrumentalness: ${features.instrumentalness}`
-    //Images.setImageWithDescription("top-song-three", trackThree, "top-song-three-name", trackThreeName, "top-song-three-artist-name", trackThreeArtist);
-    //let songThreeColors = await Images.grabImageColors("top-song-three-color-canvas", trackThreeColorImage);
-    //document.getElementById("top-song-three").style.boxShadow = `2px 1px 12px 15px rgba(${songThreeColors[0]}, 1)`;
+    //-----------------------------------------------------------------------------------------------------------------
+    let artistOneImage = artists.items[0].images[0];
+    let artistOneColorImage = artists.items[0].images[2];
+    let artistOneName = artists.items[0].name;
+    let artistTwoImage = artists.items[1].images[0];
+    let artistTwoColorImage = artists.items[1].images[2];
+    let artistTwoName = artists.items[1].name;
+    let artistThreeImage = artists.items[2].images[0];
+    let artistThreeColorImage = artists.items[2].images[2];
+    let artistThreeName = artists.items[2].name;
+    Images.setArtistImage("top-artist-one", artistOneImage, "top-artist-one-name", artistOneName);
+    let artistOneColors = await Images.grabImageColors("top-artist-one-color-canvas", artistOneColorImage);
+    document.getElementById("top-artist-one").style.boxShadow = `2px 1px 12px 15px rgba(${artistOneColors[0]}, 1)`;
+    Images.setArtistImage("top-artist-two", artistTwoImage, "top-artist-two-name", artistTwoName);
+    let artistTwoColors = await Images.grabImageColors("top-artist-two-color-canvas", artistTwoColorImage);
+    document.getElementById("top-artist-two").style.boxShadow = `2px 1px 12px 15px rgba(${artistTwoColors[0]}, 1)`;
+    Images.setArtistImage("top-artist-three", artistThreeImage, "top-artist-three-name", artistThreeName);
+    let artistThreeColors = await Images.grabImageColors("top-artist-three-color-canvas", artistThreeColorImage);
+    document.getElementById("top-artist-three").style.boxShadow = `2px 1px 12px 15px rgba(${artistThreeColors[0]}, 1)`;
     function repeat() {
         setTimeout(async () => {
             if (imageStart < songs.items.length - 1) {
